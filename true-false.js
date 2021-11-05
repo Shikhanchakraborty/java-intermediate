1e1efe83d92c4cb7306c0bec5dbb8b261654ec66
asikurrahmanshikhan <asikur.rahman987@gmail.com>
2021-11-05 12:28:39 +0600

    if and else

---
+++ true-false.js
@@ -0,0 +1,49 @@
+const name = 0;
+if (name){
+    console.log("Condition is true");
+}
+else{
+    console.log("Condition is false")
+}
+const age = 5;
+
+if(age >4){
+    console.log("condition is true")
+}
+else{
+    console.log("condition is false")
+}
+
+
+const  name = null
+console.log((name))
+const name = 5;
+if(name){
+    console.log("condition is true")
+}
+else{
+    console.log("condition is false")
+}
+
+const add = (x,y) => x+y;
+const result = add(12+56);
+console.log(result)
+
+const firstName = ""
+const lastName = "" 
+const fullName = firstName+lastName;
+console.log(fullName);
+if (fullName) {
+    console.log("Condition is true")
+}
+else{
+    console.log("Condition is false")
+}
+
+const name = true;
+if (name) {
+    console.log("condition is true");    
+}
+else{
+    console.log("condition is false")
+}
